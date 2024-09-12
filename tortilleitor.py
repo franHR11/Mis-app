3bc9f0779250ed4aea911d7d0a9e9ee6e581eaf4
franHR11 <155664460+franHR11@users.noreply.github.com>
2024-09-12 14:23:12 +0200

    Add files via upload

---
+++ tortilleitor.py
@@ -0,0 +1,17 @@
+'''
+Este es un programa para ayudarte a saber los ingredientes 
+que te hacen falta para hacer una tortilla de patatas
+
+'''
+#  presentación de los datos del programa
+
+NOMBRE_DEL_PROGRAMA = "Programa Tortilleitor"
+VERSION = "0.1"
+AUTOR = "Francisco Jose Herreros Rodriguez"
+print(NOMBRE_DEL_PROGRAMA,VERSION,AUTOR)
+
+
+Si = True
+NO = False
+print("Soy Tortilleitor, tu asistente virtual para ayudarte a saber los ingredientes que te hacen falta para hacer una tortilla de patatas: ")
+input("quieres que te ayude a contar los ingredientes que te faltan? contesta con un si/no")
